7c4f208aa1930efe34ef1e6011fb5d3f2f0892c4
Razvan Gabriel Dudau <128326189+gabrieldudau@users.noreply.github.com>
2026-08-12 23:26:35 +0200

    Add: two-integer-sum-ii - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-3.cs
@@ -0,0 +1,29 @@
+public class Solution
+{
+    public int[] TwoSum(int[] numbers, int target)
+    {
+        int left = 0;
+        int right = numbers.Length - 1; 
+
+        while (left < right)
+        {
+            int currentSum = numbers[left] + numbers[right];
+
+            if (currentSum == target)
+            {
+                return [left + 1, right + 1];
+            }
+            
+            if (currentSum < target)
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+
+        return [];
+    }
+}
